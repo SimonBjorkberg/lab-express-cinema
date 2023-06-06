934405548f259c0ae41712aaa6a7458befd414bb
Simon Björkberg <127056123+SimonBjorkberg@users.noreply.github.com>
2023-06-06 17:19:51 +0200

    iteration 2 done

--- db/index.js
+++ db/index.js
@@ -1,16 +1,33 @@
 // ℹ️ package responsible to make the connection with mongodb
 // https://www.npmjs.com/package/mongoose
 const mongoose = require("mongoose");
+const Movie = require("../models/Movie.model");
+const movies = require("../seeds/movies.seed");
 
 // ℹ️ Sets the MongoDB URI for our app to have access to it.
 // If no env has been set, we dynamically set it to whatever the folder name was upon the creation of the app
 
-const MONGO_URI = process.env.MONGODB_URI || "mongodb://127.0.0.1:27017/lab-express-cinema";
+const MONGO_URI =
+  process.env.MONGODB_URI || "mongodb://127.0.0.1:27017/lab-express-cinema";
 
 mongoose
   .connect(MONGO_URI)
   .then((x) => {
-    console.log(`Connected to Mongo! Database name: "${x.connections[0].name}"`);
+    console.log(
+      `Connected to Mongo! Database name: "${x.connections[0].name}"`
+    );
+  })
+  .then(() => {
+    movies.forEach((movie) => {
+      Movie.create({
+        title: movie.title,
+        director: movie.director,
+        stars: movie.stars,
+        img: movie.image,
+        desc: movie.description,
+        showtimes: movie.showtimes,
+      });
+    });
   })
   .catch((err) => {
     console.error("Error connecting to mongo: ", err);
---
+++ models/Movie.model.js
@@ -0,0 +1,27 @@
+const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
+
+const movieSchema = new Schema({
+  title: {
+    type: String,
+  },
+  director: {
+    type: String,
+  },
+  stars: {
+    type: [String],
+  },
+  img: {
+    type: String,
+  },
+  desc: {
+    type: String,
+  },
+  showtimes: {
+    type: [String],
+  },
+});
+
+const Movie = mongoose.model("Movie", movieSchema);
+
+module.exports = Movie;
